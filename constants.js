d78868f09204d1ebba3697a279674c0395399cd9
Anuja Pawar <anuja13122004@gmail.com>
2026-09-10 16:24:44 +0530

    Constants

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = "http://172.16.40.66:5001/api"; // production
-// export const IMAGE_URL = "http://172.16.40.66:5001";   // production
+export const API_URL = "http://172.16.40.66:5001/api"; // production
+export const IMAGE_URL = "http://172.16.40.66:5001"; // production
 
-export const API_URL = "http://192.168.0.109:5001/api";
-export const IMAGE_URL = "http://192.168.0.109:5001";
+// export const API_URL = "http://192.168.0.109:5001/api";
+// export const IMAGE_URL = "http://192.168.0.109:5001";
